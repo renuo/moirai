946457c7c4939c87d1e0d8ee762581560c90b41c
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-22 12:14:33 +0200

    Update migration

--- test/dummy/db/migrate/20241022085549_create_moirai_translations.rb
+++ test/dummy/db/migrate/20241022085549_create_moirai_translations.rb
@@ -1,6 +1,7 @@
 class CreateMoiraiTranslations < ActiveRecord::Migration[7.2]
   def change
     create_table :moirai_translations do |t|
+      t.string :file_path, null: false
       t.string :locale, null: false
       t.string :key, null: false
       t.text :value
