fa7b502e0d965d08a9fae5385b5c4489ef1fb35f
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-22 10:22:14 +0200

    Implement key flattening

--- app/controllers/moirai/pages_controller.rb
+++ app/controllers/moirai/pages_controller.rb
@@ -13,7 +13,20 @@ module Moirai
     def parse_file(path)
       is_yml_path = path.end_with?(".yml") || path.end_with?(".yaml")
       return unless is_yml_path
-      YAML.load_file(path)
+      yaml_content = YAML.load_file(path)
+      flatten_hash(yaml_content)
+    end
+
+    def flatten_hash(hash, parent_key = '', result = {})
+      hash.each do |key, value|
+        new_key = parent_key.empty? ? key.to_s : "#{parent_key}.#{key}"
+        if value.is_a?(Hash)
+          flatten_hash(value, new_key, result)
+        else
+          result[new_key] = value
+        end
+      end
+      result
     end
   end
 end
