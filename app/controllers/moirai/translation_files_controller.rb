acb72d7644b497871944be5d16a125aeddcc5327
Daniel Bengl <53896675+CuddlyBunion341@users.noreply.github.com>
2024-10-22 17:49:47 +0200

    Implement CRUD (#8)
    * Add todos
    
    * Fix show view table
    
    * Update show page
    
    * Implement CRUD
    
    * Fix linter
    
    * Add flashes, alerts, implement notice
    
    * Update crud
    
    * Refactor using callbacks
    
    * Improve alerts
    
    * Refactor alerts
    
    * Update show page
    
    * Update simplecss style
    
    * Update style
    
    * Refactor redirects
    
    * Extract FileHandler
    
    * Implement show page highlight
    
    * Add tooltip
    
    * Add translation files test
    
    * Prefix route names
    
    * Remove application system test case

--- app/controllers/moirai/translation_files_controller.rb
+++ app/controllers/moirai/translation_files_controller.rb
@@ -1,13 +1,10 @@
-require "cgi"
-require "digest"
-
 module Moirai
   class TranslationFilesController < ApplicationController
-    before_action :load_file_paths, only: [:index, :show]
-    before_action :generate_file_hashes, only: [:index, :show]
+    before_action :load_file_handler, only: [:index, :show, :create_or_update]
+    before_action :set_translation_file, only: [:show, :create_or_update]
 
     def index
-      @files = @file_paths.map do |path|
+      @files = @file_handler.file_paths.map do |path|
         {
           id: Digest::SHA256.hexdigest(path),
           name: File.basename(path),
@@ -17,48 +14,70 @@ module Moirai
     end
 
     def show
-      @file_path = @file_hashes[params[:id]]
-      decoded_path = CGI.unescape(@file_path)
-      @translation_keys = parse_file(decoded_path)
+      @translation_keys = @file_handler.parse_file(@decoded_path)
+    end
+
+    def create_or_update
+      if (translation = Translation.find_by(file_path: translation_params[:file_path], key: translation_params[:key], locale: translation_params[:locale]))
+        handle_update(translation)
+      else
+        handle_create
+      end
     end
 
     private
 
-    def load_file_paths
-      i18n_file_paths = I18n.load_path
-      @file_paths = i18n_file_paths.select { |path| path.end_with?(".yml", ".yaml") }
-    end
-
-    def generate_file_hashes
-      @file_hashes = @file_paths.map { |path| [Digest::SHA256.hexdigest(path), path] }.to_h
-    end
-
-    def parse_file(path)
-      yaml_content = YAML.load_file(path)
-      root_key = yaml_content.keys.first
-      flatten_hash(yaml_content[root_key])
-    end
-
-    def flatten_hash(hash, parent_key = "", result = {})
-      hash.each do |key, value|
-        new_key = parent_key.empty? ? key.to_s : "#{parent_key}.#{key}"
-        case value
-        when Hash
-          flatten_hash(value, new_key, result)
-        when Array
-          value.each_with_index do |item, index|
-            array_key = "#{new_key}.#{index}"
-            if item.is_a?(Hash)
-              flatten_hash(item, array_key, result)
-            else
-              result[array_key] = item
-            end
-          end
-        else
-          result[new_key] = value
-        end
+    def handle_update(translation)
+      translation_from_file = @file_handler.parse_file(@decoded_path)
+      if translation_from_file[translation.key] == translation_params[:value]
+        translation.destroy
+        flash.notice = "Translation #{translation.key} was successfully deleted."
+        redirect_to_translation_file(translation.file_path)
+        return
       end
-      result
+
+      if translation.update(value: translation_params[:value])
+        flash.notice = "Translation #{translation.key} was successfully updated."
+      else
+        flash.alert = translation.errors.full_messages.join(", ")
+      end
+
+      redirect_to_translation_file(translation.file_path)
+    end
+
+    def handle_create
+      translation_from_file = @file_handler.parse_file(@decoded_path)
+      if translation_from_file[translation_params[:key]] == translation_params[:value]
+        flash.alert = "Translation #{translation_params[:key]} already exists."
+        redirect_to_translation_file(translation_params[:file_path])
+        return
+      end
+
+      translation = Translation.new(translation_params)
+      if translation.save
+        flash.notice = "Translation #{translation.key} was successfully created."
+      else
+        flash.alert = translation.errors.full_messages.join(", ")
+      end
+
+      redirect_to_translation_file(translation.file_path)
+    end
+
+    def redirect_to_translation_file(file_path)
+      redirect_to moirai_translation_file_path(Digest::SHA256.hexdigest(file_path))
+    end
+
+    def set_translation_file
+      @file_path = @file_handler.file_hashes[params[:id]]
+      @decoded_path = CGI.unescape(@file_path)
+    end
+
+    def translation_params
+      params.require(:translation).permit(:key, :locale, :value, :file_path)
+    end
+
+    def load_file_handler
+      @file_handler = Moirai::TranslationFileHandler.new
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Moirai::Engine.routes.draw do
   root to: "pages#index"
 
-  resources :translation_files, only: %i[index show]
+  resources :translation_files, only: %i[index show], as: "moirai_translation_files"
+  post "/translation_files/:id", to: "translation_files#create_or_update", as: "moirai_create_or_update_translation"
 end
