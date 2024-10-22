ae8da1f8e462099a25086081870c122a80e27bfa
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-22 18:44:50 +0200

    Update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Moirai::Engine.routes.draw do
-  root to: "pages#index"
+  root to: "translation_files#index"
 
   resources :translation_files, only: %i[index show], as: "moirai_translation_files"
   post "/translation_files/:id", to: "translation_files#create_or_update", as: "moirai_create_or_update_translation"
