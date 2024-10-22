aba66f3be3ef7009c06501a71eaa877200dc0246
Alessandro Rodi <coorasse@gmail.com>
2024-10-22 16:58:04 +0200

    Testing stuff

--- lib/moirai/engine.rb
+++ lib/moirai/engine.rb
@@ -9,7 +9,24 @@ module Moirai
     end
 
     config.after_initialize do
+      # I18n.backend.extend(I18n::Backend::MoiraiTags)
       I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Moirai.new, I18n.backend)
+    end
+
+    initializer 'moirai.override_translation_helper' do
+      ActiveSupport.on_load(:action_view) do
+        module ActionView::Helpers::TranslationHelper
+          def translate(key, **options)
+            # Call the original method to retain full functionality
+            result = super
+
+            # Wrap the result in your custom tag
+            content_tag(:span, result, class: 'custom-translation')
+          end
+
+          alias :t :translate
+        end
+      end
     end
   end
 end
