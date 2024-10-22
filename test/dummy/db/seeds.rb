0c9bda7f6ca1bfccf9fe8417ed6aa6122359b98b
Alessandro Rodi <coorasse@gmail.com>
2024-10-22 14:45:54 +0200

    Add seeds (#7)
    * Add seeds
    
    * Add meaningful seeds

---
+++ test/dummy/db/seeds.rb
@@ -0,0 +1,3 @@
+Moirai::Translation.create!(file_path: "config/locales/it.yml", locale: "it", key: "locales.german", value: "Germanico")
+Moirai::Translation.create!(file_path: "config/locales/it.yml", locale: "it", key: "locales.italian", value: "Italianico")
+Moirai::Translation.create!(file_path: "config/locales/it.yml", locale: "it", key: "locales.english", value: "Inglesofilo")
