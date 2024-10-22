35afd1823ed57e79514c3dfa2e3ae227b3109f41
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-22 12:17:10 +0200

    Update validations

--- app/models/moirai/translation.rb
+++ app/models/moirai/translation.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 module Moirai
   class Translation < Moirai::ApplicationRecord
+    validates_presence_of :key, :locale, :file_path
+    validate :file_path_must_exist
+
+    private
+
+    def file_path_must_exist
+      errors.add(:file_path, "must exist") unless File.exist?(file_path)
+    end
   end
 end
