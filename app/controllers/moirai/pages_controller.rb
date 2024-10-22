13c9c14f6d022deb8777f038c9764497a1fbdd73
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-22 10:20:35 +0200

    Implement yaml parsing

--- app/controllers/moirai/pages_controller.rb
+++ app/controllers/moirai/pages_controller.rb
@@ -3,7 +3,17 @@
 module Moirai
   class PagesController < ApplicationController
     def index
-      render plain: "Hello World"
+      i18n_file_paths = I18n.load_path
+      file_contents = i18n_file_paths.map { |path| [path, parse_file(path)] }.to_h
+      render json: file_contents
+    end
+
+    private
+
+    def parse_file(path)
+      is_yml_path = path.end_with?(".yml") || path.end_with?(".yaml")
+      return unless is_yml_path
+      YAML.load_file(path)
     end
   end
 end
