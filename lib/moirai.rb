d37e3977777dff07c885222a7c31779466768ffb
Alessandro Rodi <coorasse@gmail.com>
2024-10-23 09:21:07 +0200

    Remove require of unused file

--- lib/moirai.rb
+++ lib/moirai.rb
@@ -4,7 +4,6 @@ require "moirai/version"
 require "moirai/engine"
 require "moirai/pull_request_creator"
 require "i18n/backend/moirai"
-require "i18n/backend/moirai_tags"
 
 module Moirai
   # Your code goes here...
