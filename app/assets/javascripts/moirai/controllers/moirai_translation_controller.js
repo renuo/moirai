6afbeb4b96c60ffbf46e154f5ed514c6d36b2567
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-23 09:59:41 +0200

    Implement inline editing fr

--- app/assets/javascripts/moirai/controllers/moirai_translation_controller.js
+++ app/assets/javascripts/moirai/controllers/moirai_translation_controller.js
@@ -1,8 +1,40 @@
 Stimulus.register(
   "moirai-translation",
   class extends Controller {
+    static targets = ["inlineTranslation"]
+
     connect() {
-      console.log("Trans");
+      console.log(this.inlineTranslationTargets);
+    }
+
+    submit(event) {
+      console.log(event.target)
+      const {filePath, key} = event.target.dataset
+      console.log(filePath, key, event.target.dataset)
+
+      const csrfToken = document.querySelector('meta[name="csrf-token"]').content  
+
+      console.log("CSRF TOKEN", csrfToken)
+
+      const formObject = new FormData()
+      formObject.append('translation[key]', key)
+      formObject.append('translation[file_path]', filePath)
+      formObject.append('translation[value]', event.target.innerText)
+
+      console.log({formObject})
+
+      console.log("FOOBAR")
+
+
+      fetch(`/moirai/translation_files`, {
+        method: 'POST',
+        headers: {
+          'X-CSRF-Token': csrfToken
+        },
+        body: formObject
+      }).then(response => {
+      }).catch(error => {
+      })
     }
   }
 );
