fabde872f91c5ff6ebec294156a0fe5fde63b9ac
Alessandro Rodi <coorasse@gmail.com>
2024-10-28 18:06:21 +0100

    Add tests for the TranslationDumper (#37)

--- app/models/moirai/translation_dumper.rb
+++ app/models/moirai/translation_dumper.rb
@@ -1,14 +1,17 @@
 module Moirai
   class TranslationDumper
+    # @return Array[Hash]
     def call
-      project_root = Rails.root.to_s
+      project_root = Rails.root
       Moirai::Translation.pluck(:file_path).uniq.map do |file_path|
         absolute_file_path = File.expand_path(file_path, project_root)
-        next unless absolute_file_path.start_with?(project_root)
+
+        # skip file paths that don't belong to the project
+        next unless absolute_file_path.start_with?(project_root.to_s)
 
         updated_file_contents = get_updated_file_contents(file_path)
         {
-          file_path: file_path.sub(project_root, "."),
+          file_path: Pathname.new(file_path).relative_path_from(project_root),
           content: updated_file_contents
         }
       end.compact
