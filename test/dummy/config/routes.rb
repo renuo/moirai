320ad0b528b91dbfd739d1f6977cdc7e3e94f122
Alessandro Rodi <coorasse@gmail.com>
2024-10-30 21:00:49 +0100

    Fix linting

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Moirai::Engine => "/moirai", as: "moirai"
+  mount Moirai::Engine => "/moirai", :as => "moirai"
   root to: "home#index"
 end
