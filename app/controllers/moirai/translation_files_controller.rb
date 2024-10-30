c9351ef55f26fe85c2c5c2c922798cd398ead2da
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-30 10:11:19 +0100

    Add tests for show action

--- app/controllers/moirai/translation_files_controller.rb
+++ app/controllers/moirai/translation_files_controller.rb
@@ -16,7 +16,7 @@ module Moirai
     def show
       @translation_keys = @file_handler.parse_file(@decoded_path)
       @locale = @file_handler.get_first_key(@decoded_path)
-      @translations = Moirai::Translation.find_by_file_path(@decoded_path)
+      @translations = Moirai::Translation.by_file_path(@decoded_path)
     end
 
     def create_or_update
@@ -91,6 +91,11 @@ module Moirai
 
     def set_translation_file
       @file_path = @file_handler.file_hashes[params[:id]]
+      if @file_path.nil?
+        flash.alert = "File not found"
+        redirect_to moirai_translation_files_path, status: :not_found
+        return
+      end
       @decoded_path = CGI.unescape(@file_path)
     end
 
--- test/controllers/moirai/translation_files_controller_test.rb
+++ test/controllers/moirai/translation_files_controller_test.rb
@@ -5,7 +5,7 @@ class TranslationFilesController < ActionDispatch::IntegrationTest
     @translation = Moirai::Translation.create(key: "test_key", locale: "en", value: "test_value")
   end
 
-  test "should get index" do
+  test "#index" do
     get "/moirai"
 
     assert_response :success
@@ -14,5 +14,18 @@ class TranslationFilesController < ActionDispatch::IntegrationTest
     assert_includes response.body, "de.yml"
     assert_includes response.body, "en.yml"
     assert_includes response.body, "it.yml"
+  end
+
+  test "#show exists" do
+    get "/moirai/translation_files/#{Digest::SHA256.hexdigest(Rails.root.join("config/locales/en.yml").to_s)}"
+    assert_response :success
+
+    assert_select "h1", "Update translations"
+    assert_select "code", "./config/locales/en.yml"
+  end
+
+  test "#show does not exist" do
+    get "/moirai/translation_files/does_not_exist"
+    assert_response :not_found
   end
 end
