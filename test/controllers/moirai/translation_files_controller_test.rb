b6e0e4b174deb65dc966b1ba5c9e9bde8af29dc4
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-30 13:23:02 +0100

    Fix linter

--- test/controllers/moirai/translation_files_controller_test.rb
+++ test/controllers/moirai/translation_files_controller_test.rb
@@ -33,19 +33,20 @@ class TranslationFilesController < ActionDispatch::IntegrationTest
 
   test "create with valid params" do
     translation_count_before = Moirai::Translation.count
-    post "/moirai/translation_files", params: { translation: { key: "locales.german", locale: "en", value: "New Translation" } }
+    post "/moirai/translation_files", params: {translation: {key: "locales.german", locale: "en", value: "New Translation"}}
 
     assert_response :redirect
     assert_redirected_to translation_file_url("config/locales/en.yml")
     assert_equal "Translation locales.german was successfully created.", flash[:notice]
     assert_equal Moirai::Translation.last.key, "locales.german"
     assert_equal Moirai::Translation.last.value, "New Translation"
+    assert_equal translation_count_before + 1, Moirai::Translation.count
   end
 
   test "create with same value" do
     translation_count_before = Moirai::Translation.count
 
-    post "/moirai/translation_files", params: { translation: { key: "locales.german", locale: "en", value: "German" } }
+    post "/moirai/translation_files", params: {translation: {key: "locales.german", locale: "en", value: "German"}}
 
     assert_response :redirect
     assert_redirected_to translation_file_url("config/locales/en.yml")
@@ -56,7 +57,7 @@ class TranslationFilesController < ActionDispatch::IntegrationTest
   test "create with invalid params" do
     translation_count_before = Moirai::Translation.count
 
-    post "/moirai/translation_files", params: { translation: { key: "", locale: "", value: "" } }
+    post "/moirai/translation_files", params: {translation: {key: "", locale: "", value: ""}}
 
     assert_response :unprocessable_entity
     assert_equal "Key can't be blank, Locale can't be blank", flash[:alert]
@@ -67,7 +68,7 @@ class TranslationFilesController < ActionDispatch::IntegrationTest
 
   test "update with blank value" do
     count_before = Moirai::Translation.count
-    post "/moirai/translation_files", params: { translation: { key: "locales.german", locale: "de", value: "" } }
+    post "/moirai/translation_files", params: {translation: {key: "locales.german", locale: "de", value: ""}}
 
     assert_response :redirect
     assert_redirected_to translation_file_url("config/locales/de.yml")
@@ -76,7 +77,7 @@ class TranslationFilesController < ActionDispatch::IntegrationTest
   end
 
   test "update with non blank new value" do
-    post "/moirai/translation_files", params: { translation: { key: "locales.german", locale: "de", value: "Hochdeutsch" } }
+    post "/moirai/translation_files", params: {translation: {key: "locales.german", locale: "de", value: "Hochdeutsch"}}
 
     assert_response :redirect
     assert_redirected_to translation_file_url("config/locales/de.yml")
@@ -87,7 +88,7 @@ class TranslationFilesController < ActionDispatch::IntegrationTest
 
   test "update with value from file" do
     count_before = Moirai::Translation.count
-    post "/moirai/translation_files", params: { translation: { key: "locales.german", locale: "de", value: "Deutsch" } }
+    post "/moirai/translation_files", params: {translation: {key: "locales.german", locale: "de", value: "Deutsch"}}
 
     assert_response :redirect
     assert_redirected_to translation_file_url("config/locales/de.yml")
