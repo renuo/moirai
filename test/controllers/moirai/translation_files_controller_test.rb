fb3091248f0cbd0b3f5ad40fd43d5dfa978715cf
Daniel Bengl <daniel.bengl@renuo.ch>
2024-10-30 11:20:47 +0100

    Update translation controllers spec

--- test/controllers/moirai/translation_files_controller_test.rb
+++ test/controllers/moirai/translation_files_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TranslationFilesController < ActionDispatch::IntegrationTest
   setup do
-    @translation = Moirai::Translation.create(key: "test_key", locale: "en", value: "test_value")
+    @translation = Moirai::Translation.create(key: "locales.german", locale: "de", value: "Neudeutsch")
   end
 
   test "#index" do
@@ -37,7 +37,28 @@ class TranslationFilesController < ActionDispatch::IntegrationTest
     assert_response :redirect
     assert_redirected_to translation_file_url("config/locales/en.yml")
     assert_equal "Translation locales.german was successfully created.", flash[:notice]
-    assert_equal translation_count_before + 1, Moirai::Translation.count
+    assert_equal Moirai::Translation.last.key, "locales.german"
+    assert_equal Moirai::Translation.last.value, "New Translation"
+  end
+
+  test "#create_or_update with existing translation record" do
+    post "/moirai/translation_files", params: { translation: { key: "locales.german", locale: "de", value: "Hochdeutsch" } }
+
+    assert_response :redirect
+    assert_redirected_to translation_file_url("config/locales/de.yml")
+    assert_equal "Translation locales.german was successfully updated.", flash[:notice]
+    assert_equal Moirai::Translation.last.key, "locales.german"
+    assert_equal Moirai::Translation.last.value, "Hochdeutsch"
+  end
+
+  test "#create_or_update for existing translation with empty translation value restores translation from file" do
+    count_before = Moirai::Translation.count
+    post "/moirai/translation_files", params: { translation: { key: "locales.german", locale: "de", value: "" } }
+
+    assert_response :redirect
+    assert_redirected_to translation_file_url("config/locales/de.yml")
+    assert_equal "Translation locales.german was successfully deleted.", flash[:notice]
+    assert_equal count_before - 1, Moirai::Translation.count
   end
 end
 
