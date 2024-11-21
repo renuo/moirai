2c1235b714c656fd25d9d7690c570d0e5599aa3d
oliveranthony17 <oliver.h.anthony@gmail.com>
2024-11-21 13:59:01 +0100

    Remove debugging from I18n_test

--- test/i18n/i18n_test.rb
+++ test/i18n/i18n_test.rb
@@ -4,16 +4,6 @@ require "test_helper"
 
 class I18nExtensionsTest < ActiveSupport::TestCase
   test "it correctly translates using .translate and .translate_without_moirai" do
-    # Debugging: Check if the database table exists
-    table_exists = ActiveRecord::Base.connection.data_source_exists?("moirai_translations")
-    Rails.logger.debug("Database connection established: #{ActiveRecord::Base.connected?}")
-    Rails.logger.debug("moirai_translations table exists: #{table_exists}")
-    Rails.logger.debug("Original backend is set: #{I18n.original_backend.present?}")
-    Rails.logger.debug(I18n.backend.inspect)
-
-    # Ensure the table exists, or raise an error to debug further
-    assert table_exists, "The moirai_translations table does not exist in the test database."
-
     assert_equal "Italienisch", I18n.t("locales.italian", locale: :de)
 
     Moirai::Translation.create!(locale: "de", key: "locales.italian", value: "Italianese")
